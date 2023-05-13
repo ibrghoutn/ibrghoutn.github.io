b16ca16985160b7c47208c24443b99892c0e51d9
h.ibrghoutn <hamza.ibrghoutn@gmail.com>
2023-05-13 16:35:21 +0100

    deploy me9sous

--- me9sous/assets/distance.101d251c.js
+++ me9sous/assets/distance.101d251c.js
@@ -1,1 +1,1 @@
-const s="/assets/location.02d53c60.svg",t="/assets/distance.84ddba44.svg";export{s as _,t as a};
+const s="/me9sous/assets/location.02d53c60.svg",o="/me9sous/assets/distance.84ddba44.svg";export{s as _,o as a};
